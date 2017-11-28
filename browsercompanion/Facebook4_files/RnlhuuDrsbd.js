352079a28c8615d622a53f8e8da58effd844a83a
nanashiivy <Esther@esther.local>
2017-11-29 00:17:29 +0100

    actual update

---
+++ browsercompanion/Facebook4_files/RnlhuuDrsbd.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["KDPcu"]); }
+
+__d("XTipJarCommentPreviewController",["XController"],(function a(b,c,d,e,f,g){f.exports=c("XController").create("/tip_jar/live_comment_attachment/",{})}),null);
